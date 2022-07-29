d28b409057cc01b212af6fe1717189aa667d0e12
parth <parthtrivedi0710@gmail.com>
2022-07-29 20:56:41 +0530

    ..

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,3 +1,7 @@
+// Passport is authentication middleware for Node. js. 
+// Extremely flexible and modular, Passport can be unobtrusively dropped in to any Express-based web application.
+// A comprehensive set of strategies support authentication using a username and password, Facebook, Twitter, and more.
+
 const passport = require("passport");
 const localStrategy = require("passport-local").Strategy;
 const User = require("../models/User");
@@ -12,14 +16,14 @@ passport.use(
             try {
                 const user = await User.findOne({ email });
                 if (!user) {
-                    return done(null, false, { message: "User not found" });
+                    return done(null, false, { message: "User Not Found" });
                 }
 
                 const validate = await user.isValidPassword(password);
                 if (!validate) {
                     return done(null, false, { message: "Wrong Password" });
                 }
-                return done(null, user, { message: "Logged in Successfully" });
+                return done(null, user, { message: "Logged In Successfully" });
             } catch (error) {
                 return done(error);
             }
--- frontend/src/components/API/api.js
+++ frontend/src/components/API/api.js
@@ -1,3 +1,8 @@
+//Axios: Axios is a Javascript library used to make HTTP requests
+// from node. js or XMLHttpRequests from the browser and it supports the Promise API
+// that is native to JS ES6. It can be used intercept HTTP requests and responses and 
+//enables client-side protection against XSRF.
+
 import axios from 'axios'
 export default axios.create({
     baseURL: 'http//localhost:8080'
